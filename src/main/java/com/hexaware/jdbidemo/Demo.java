4f91349d67efc53a9131ad84af68b7a24d6677bb
pradeep.kumar44@gmail.com <pradeep.kumar44@gmail.com>
2018-04-04 11:53:07 +0530

    committing the changes with Response entity.

---
+++ src/main/java/com/hexaware/jdbidemo/Demo.java
@@ -0,0 +1,23 @@
+package com.hexaware.jdbidemo;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Demo {
+
+	public static void main(String[] args) throws Exception {
+        TreeSet<Integer> set = new TreeSet<Integer>();
+        set.add(2);
+        set.add(56);
+        set.add(12);
+        set.add(6);
+        set.add(20);
+        set.add(27);
+
+		
+        System.out.println(set);
+
+	}
+}
--- src/main/java/com/hexaware/jdbidemo/PersonRest.java
+++ src/main/java/com/hexaware/jdbidemo/PersonRest.java
@@ -4,10 +4,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
+
 import java.util.List;
 
 
@@ -16,22 +18,31 @@ public class PersonRest {
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
-  public final List<Person> fetchAll() {
+  public final Response fetchAll() {
     List<Person> personList = Person.listAllPerson();
-    return personList;
+    return Response.ok().entity(personList).build();
   }
 
   @GET
   @Path("{id}")
   @Produces(MediaType.APPLICATION_JSON)
-  public final Person findById(@PathParam("id") final int id) {
+  public final Response findById(@PathParam("id") final int id) {
     final Person person = Person.findPersonById(id);
-    return person;
+    if (person == null){
+      return Response.status(Response.Status.NOT_FOUND).entity("{\"error\":\"Sorry, No such person found\"}").build();
+    }
+    return Response.ok().entity(person).build();
   }  
 
   @POST
   @Consumes(MediaType.APPLICATION_JSON) 
-  public final void insert(Person person) {
-    Person.insertIntoPersonTable(person);
+  public final Response insert(Person person) {
+    int id = Person.insertIntoPersonTable(person);
+    if (id < 0 ){
+      return Response.status(Response.Status.NOT_FOUND).entity("{\"error\":\"Could not insert the person record\"}").build();
+    }else{
+      return Response.status(Response.Status.CREATED).ok().build();
+    }
+
   }
 }
